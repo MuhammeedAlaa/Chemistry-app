e478276ba43dc14e5fb8163367998c3218574462
davidraafat <45523625+davidraafat@users.noreply.github.com>
2020-01-31 15:32:01 +0200

    lecture view

--- routes/assistant.js
+++ routes/assistant.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { isauth } = require('../utils/auth');
+const { insertLecture } = require('../databaseUtils/insert');
 const router = express.Router();
+
 router.get('/', function(req, res) {
     const { role } = isauth(req);
     if (role == 'assistant') {
@@ -9,4 +11,18 @@ router.get('/', function(req, res) {
         res.redirect('/');
     }
 });
+
+router.post("/AddLecture", function(req, res) {
+    console.log(req.body);
+    const { role } = isauth(req);
+    if (role == 'assistant') {
+        console.log(req.body);
+        insertLecture(req.center_name, req.course_id, req.day, req.hour);
+        res.redirect("/");
+    } else {
+        res.redirect('/');
+    }
+    res.redirect("/");
+});
+
 module.exports = router;
